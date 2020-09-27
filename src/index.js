c8db64ca6d041a0dab16493b08539197c772cfb4
sousakt <sousakt@gmail.com>
2020-09-27 15:21:57 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
+  // noi dung in ra html
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  //vi tri in ra html
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
