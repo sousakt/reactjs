fc8d4bb8050d4d92fa14546db090f3a2f989e5b1
sousakt <sousakt@gmail.com>
2020-10-03 14:28:20 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import StateLessComponent from "./components/StateLessComponent";
 import StateFull, { StateFullComponent } from "./components/StateFullComponent";
-
+import Home from "./baiTapChiaComponent/Home";
 function App() {
   return (
     <div>
-      <StateLessComponent />
-      <StateFull />
+      {/* <StateLessComponent />
+      <StateFull /> */}
+      <Home />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+//import css js của bootstrap
+//css
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+//js
+import "../node_modules/bootstrap/dist/js/bootstrap.min";
+import "../node_modules/jquery/dist/jquery.slim.min";
+import "../node_modules/popper.js/dist/umd/popper.min";
+
 ReactDOM.render(
   // noi dung in ra html
   <React.StrictMode>
